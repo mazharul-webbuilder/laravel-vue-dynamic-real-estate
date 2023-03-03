0538f33fa026c5a6a7a2d8303425c863043f7c10
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-03-03 21:34:39 +0600

    reactive variabel

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,13 +1,12 @@
 <template>
-    <h1 class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, ipsa?</h1>
-    <Link href="/hello">Show Page</Link>
-    <div>
-        The message is {{ message }}
-    </div>
+    <MainLayout>
+
+    </MainLayout>
 </template>
 
 <script setup>
     import {Link} from '@inertiajs/vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue'
     // defineProps(['message'])
     defineProps({
         message: String
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,7 +1,12 @@
 <template>
-    <h1 class="">Show</h1>
-    <Link href="/">Main Page</Link>
+    <MainLayout>
+
+    </MainLayout>
+    <slot>
+        Helo from shw pagefadf dsff  fasd dfas
+    </slot>
 </template>
 <script setup>
     import {Link} from '@inertiajs/vue3'
+    import MainLayout from '../../Layouts/MainLayout.vue'
 </script>
